71da38b4bd86c2eed24d055a3c1fdacbb56a50d8
lleviathe <lleviathe@gmail.com>
2022-07-17 23:27:11 +0400

    setup tests

--- src/tests/Feature/Api/V1/CandidateTest.php
+++ src/tests/Feature/Api/V1/CandidateTest.php
@@ -2,16 +2,22 @@
 
 namespace Tests\Feature\Api\V1;
 
+use App\Models\Candidate;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CandidateTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_can_retrieve_candidates()
     {
+        Candidate::factory()->create();
+
         $response = $this->getJson(route('api.v1.candidates.index'));
 
         $response->assertStatus(200);
+        $response->assertJsonCount(1);
     }
 }
